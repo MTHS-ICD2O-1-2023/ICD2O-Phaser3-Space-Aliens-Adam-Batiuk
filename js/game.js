d05f31c74861c4c6f96d3d55f771a0bfc5bc8c1b
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-04-17 17:55:07 +0000

    started game.js

---
+++ js/game.js
@@ -0,0 +1,29 @@
+/* global Phaser */
+
+// Created by Adam Batiuk
+// Created on April 17 2023
+// game config
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  //set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    made: Phaser.Scale.FIT,
+    // place in middle
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
