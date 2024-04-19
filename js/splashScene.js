4fd0f74975c0ef725bed0b1a4cac0d9947f16d2b
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-04-19 18:17:47 +0000

    splashse=cenee

---
+++ js/splashScene.js
@@ -0,0 +1,56 @@
+/* global Phaser */
+
+// Created by Adam Batiuk
+// Created on April 17 2023
+// This is a Splash Scene
+
+/**
+ * This class is the Splash Scene.
+ */
+class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the construtor
+   */
+  constructor() {
+    super({ key: "splashScene"})
+  }
+
+  /**
+   * Can be defined on own Scenes.
+   * This method is call by the Scence Manager when starts,
+   *   before preload() and create()
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColof("ffffff")
+  }
+
+  /**
+   * Can be defined on own Scenes.
+   * Use to load assets.
+   */
+  preload() {
+    console.log("splash Scene")
+  }
+
+  /**
+   * Can be defined on own Scenes.
+   * Use to create game opjects.
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+  create(data) {
+    // pass
+  }
+
+  /**
+   * Should be overridden
+   * This method is called once per game step
+   * @param {number} time - The current time
+   * @param {number} delta- The delta time since last frame (ms)
+   */
+  update(time, delta) {
+    //pass
+  }
+}
+
+export default SplashScene
