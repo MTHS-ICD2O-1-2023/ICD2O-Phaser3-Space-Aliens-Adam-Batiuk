360de48c56dabc5f14739b0c484d9149e6314633
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-12 15:29:03 +0000

    01

--- js/game.js
+++ js/game.js
@@ -1,48 +1,11 @@
 /* global Phaser */
 
-// Created by Adam Batiuk
-// Created on April 17 2023
-// game config
-
-// scene import
-import SplashScene from "./splashScene.js"
-import TitleScene from "./titleScene.js"
-import MenuScene from "./menuScene.js"
-
-// create the new scenes
-const splashScene = new SplashScene()
-const titleScene = new TitleScene()
-const menuScene = new MenuScene()
-
-/**
- * Start Phaser Game.
- */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: true,
-    },
-  },
-  //set background color
-  backgroundColor: 0x5f6e7a,
-  scale: {
-    made: Phaser.Scale.FIT,
-    // place in middle
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
+  backgroundColor: 0x5f6e7a
 }
 
 const game = new Phaser.Game(config)
-// console.log(game)
-
-// load scenes
-game.scene.add("splashScene", splashScene)
-game.scene.add("titleScene", titleScene)
-game.scene.add("menuScene", menuScene)
-
-// the star scene
-game.scene.start("splashScene")
+console.log(game)
