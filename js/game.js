1140ec2001f26d716c32736026eada703f994079
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-12 15:45:29 +0000

    02

--- js/game.js
+++ js/game.js
@@ -4,7 +4,18 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set beckground color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
