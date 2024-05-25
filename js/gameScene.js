50eb4decdea8673199456c3b5c46c9bdb8455994
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-25 19:56:28 +0000

    13

--- js/gameScene.js
+++ js/gameScene.js
@@ -36,6 +36,7 @@ class GameScene extends Phaser.Scene {
     this.load.image('alien', 'assets/alien.png')
     // sound
     this.load.audio('laser', 'assets/laser1.wav')
+    this.load.audio('explosion', 'assets/barrelExploding.wav')
   }
 
   create(data) {
@@ -48,8 +49,15 @@ class GameScene extends Phaser.Scene {
 
     this.alienGroup = this.add.group()
     this.createAlien()
-  }
 
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
+  }
   update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
